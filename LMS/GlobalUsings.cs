0cadd08d686216b8942439de0503cb97bc51e680
Mahmoud Helmy <92005892+mahmoudhelmyofficial@users.noreply.github.com>
2026-02-25 10:04:57 +0200

    Add files via upload

---
+++ LMS/GlobalUsings.cs
@@ -0,0 +1,52 @@
+// Global using directives - تسهيل الاستيرادات العامة
+
+// System
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.ComponentModel.DataAnnotations;
+global using System.Linq.Expressions;
+
+// Microsoft Extensions
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Options;
+
+// ASP.NET Core
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Identity;
+
+// Entity Framework
+global using Microsoft.EntityFrameworkCore;
+
+// LMS Common
+global using LMS.Common;
+global using LMS.Data;
+global using LMS.Domain.Entities;
+global using LMS.Domain.Entities.Users;
+global using LMS.Domain.Entities.Courses;
+global using LMS.Domain.Entities.Learning;
+global using LMS.Domain.Entities.Assessments;
+global using LMS.Domain.Entities.Payments;
+global using LMS.Domain.Entities.Financial;
+global using LMS.Domain.Entities.Marketing;
+global using LMS.Domain.Entities.Gamification;
+global using LMS.Domain.Entities.Social;
+global using LMS.Domain.Entities.Notifications;
+global using LMS.Domain.Entities.Analytics;
+global using LMS.Domain.Entities.Settings;
+global using LMS.Domain.Entities.Security;
+global using LMS.Domain.Entities.Support;
+global using LMS.Domain.Entities.Certifications;
+global using LMS.Domain.Entities.Content;
+global using LMS.Domain.Entities.LiveSessions;
+global using LMS.Domain.Enums;
+global using LMS.Services.Interfaces;
+
+// This file contains global using directives that are automatically 
+// imported across all files in this project, reducing repetitive imports.
+
